0b9adf02a8a4c4a9a4697bc63242ab67f14cb9d1
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2025-02-21 23:24:23 +1000

    created history page

---
+++ src/app/history/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+import Header from "@/components/Header";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+export default function history() {
+  const [date, setDate] = useState("");
+  const [dishName, setDishName] = useState("");
+  const [note, setNote] = useState("");
+  const [records, setRecords] = useState<
+    { date: string; dishName: string; note: string }[]
+  >([]);
+
+  // 初回レンダリング時に localStorage からデータを読み込む
+  useEffect(() => {
+    const savedRecords = localStorage.getItem("dinner_records");
+    if (savedRecords) {
+      setRecords(JSON.parse(savedRecords));
+    }
+  }, []);
+
+  return (
+    <div className="m-0 flex flex-col items-center justify-center min-w-[320px] min-h-screen">
+      <Header />
+      <h2 className="text-3xl font-bold mb-8"> ごはんの記録</h2>
+      {records.length === 0 ? (
+        <p>記録がありません</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>日付</th>
+              <th>料理名</th>
+              <th>メモ</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map((record, index) => (
+              <tr key={index}>
+                <td>{record.date}</td>
+                <td>{record.dishName}</td>
+                <td>{record.note}</td>
+                <td>
+                  {/* <button onClick={() => handleEdit(record)}>編集</button> */}
+                  <button>編集</button>
+                </td>
+                <td>
+                  {/* <button onClick={() => handleDelete(record.id)}>削除</button> */}
+                  <button>削除</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <Link href="/">
+        <button className="mt-8 mx-4 py-3 px-5 rounded-2xl bg-gray-100 hover:bg-gray-200">
+          戻る
+        </button>
+      </Link>
+    </div>
+  );
+}
