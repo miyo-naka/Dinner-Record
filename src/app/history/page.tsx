689e729f6159db9178b0008cb8e121af9e9c3a48
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2025-02-24 11:33:41 +1000

    added pagenation

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -4,20 +4,34 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 export default function history() {
-  const [date, setDate] = useState("");
-  const [dishName, setDishName] = useState("");
-  const [note, setNote] = useState("");
+  // const [date, setDate] = useState("");
+  // const [dishName, setDishName] = useState("");
+  // const [note, setNote] = useState("");
   const [records, setRecords] = useState<
     { date: string; dishName: string; note: string }[]
   >([]);
 
-  // 初回レンダリング時に localStorage からデータを読み込む
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const recordsPerPage = 7;
+
+  // 初回レンダリング時にlocalStorageからデータを読み込む+ページネーション
   useEffect(() => {
     const savedRecords = localStorage.getItem("dinner_records");
     if (savedRecords) {
-      setRecords(JSON.parse(savedRecords));
+      const allRecords = JSON.parse(savedRecords);
+      const totalPageNumber = Math.ceil(allRecords.length / recordsPerPage);
+      setTotalPages(totalPageNumber);
+
+      //現在のページデータのみ読み込む
+      const startIndex = (page - 1) * recordsPerPage;
+      const paginatedRecords = allRecords.slice(
+        startIndex,
+        startIndex + recordsPerPage
+      );
+      setRecords(paginatedRecords);
     }
-  }, []);
+  }, [page]);
 
   return (
     <div className="m-0 flex flex-col items-center justify-center min-w-[320px] min-h-screen">
@@ -26,7 +40,7 @@ export default function history() {
       {records.length === 0 ? (
         <p>記録がありません</p>
       ) : (
-        <table className="w-[60%] mb-5">
+        <table className="w-[70%] mb-5">
           <thead>
             <tr className="[&>th]:h-10 text-left">
               <th>日付</th>
@@ -40,7 +54,7 @@ export default function history() {
             {records.map((record, index) => (
               <tr
                 key={index}
-                className="[&>td]:h-8 p-2 text-left border-t-[1px] border-t-dotted border-violet-300"
+                className="[&>td]:h-8 p-4 text-left border-t-[1px] border-t-dotted border-violet-300"
               >
                 <td>{record.date}</td>
                 <td>{record.dishName}</td>
@@ -62,9 +76,28 @@ export default function history() {
           </tbody>
         </table>
       )}
+      <div>
+        <button
+          hidden={page === 1}
+          onClick={() => setPage(page - 1)}
+          className="my-2 mx-3 py-3 px-3 rounded-2xl bg-gray-100 hover:bg-gray-200"
+        >
+          Prev
+        </button>
+        <span>
+          {page} / {totalPages}
+        </span>
+        <button
+          hidden={page === totalPages}
+          onClick={() => setPage(page + 1)}
+          className="my-2 mx-3 py-3 px-3 rounded-2xl bg-gray-100 hover:bg-gray-200"
+        >
+          Next
+        </button>
+      </div>
       <Link href="/">
         <button className="mt-8 mx-4 py-3 px-5 rounded-2xl bg-gray-100 hover:bg-gray-200">
-          戻る
+          ホームに戻る
         </button>
       </Link>
     </div>
