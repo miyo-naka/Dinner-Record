88895d4ac130db39a09556390048ebd43abb2a05
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2025-04-03 23:29:29 +1000

    fixed sort and pagination issues

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -2,23 +2,19 @@ import React, { useEffect, useState } from "react";
 import { Record } from "@/utils/recordUtils";
 
 type PaginationProps = {
-  records: Record[]; // 外部からデータを受け取る
-  onPageChange: (paginatedRecords: Record[]) => void; // ページ変更時にデータを渡す
+  totalRecords: number;
+  recordsPerPage: number;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
 };
 
-export default function Pagination({ records, onPageChange }: PaginationProps) {
-  const [page, setPage] = useState(1);
-  const recordsPerPage = 7;
-  const totalPages = Math.ceil(records.length / recordsPerPage);
-
-  //ページネーション用のデータの更新
-  useEffect(() => {
-    const paginated = records.slice(
-      (page - 1) * recordsPerPage,
-      page * recordsPerPage
-    );
-    onPageChange(paginated);
-  }, [records, page, onPageChange]);
+export default function Pagination({
+  totalRecords,
+  recordsPerPage,
+  currentPage,
+  setCurrentPage,
+}: PaginationProps) {
+  const totalPages = Math.ceil(totalRecords / recordsPerPage);
 
   return (
     <>
@@ -26,18 +22,18 @@ export default function Pagination({ records, onPageChange }: PaginationProps) {
       {totalPages > 1 && (
         <div>
           <button
-            hidden={page === 1}
-            onClick={() => setPage(page - 1)}
+            hidden={currentPage === 1}
+            onClick={() => setCurrentPage(currentPage - 1)}
             className="my-2 mx-3 py-3 px-3 rounded-2xl bg-gray-100 hover:bg-gray-200"
           >
             Prev
           </button>
           <span>
-            {page} / {totalPages} ページ
+            {currentPage} / {totalPages} ページ
           </span>
           <button
-            hidden={page === totalPages}
-            onClick={() => setPage(page + 1)}
+            hidden={currentPage === totalPages}
+            onClick={() => setCurrentPage(currentPage + 1)}
             className="my-2 mx-3 py-3 px-3 rounded-2xl bg-gray-100 hover:bg-gray-200"
           >
             Next
